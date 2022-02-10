62f5127757545a571e91724d4f519fdd9e7d0130
SuatAlpay <ssalpay96@gmail.com>
2022-02-10 15:02:43 -0500

    Added Driver Utility fallows Singleton Desing Pattern

--- src/test/java/googleTranslate/utilities/Driver.java
+++ src/test/java/googleTranslate/utilities/Driver.java
@@ -1,15 +1,44 @@
 package googleTranslate.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
 
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
     private Driver() {
     }
 
-    public static void getDriver(){
+    public static WebDriver getDriver(){
 
+        if (driverPool.get()==null){
+
+            String browser = System.getProperty("browser") != null ? System.getProperty("browser") : ConfigurationReader.getProperty("browser");
+
+            switch (browser){
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driverPool.set(new ChromeDriver());
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    break;
+            }
+        }
+
+        return driverPool.get();
+    }
+
+    public static void closeDriver(){
+
+        if (driverPool.get()!=null){
+            driverPool.get().quit();
+            driverPool.get().close();
+        }
     }
 }
