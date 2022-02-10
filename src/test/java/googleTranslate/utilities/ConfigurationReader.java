1778c8f673ec64826dc963da35230d601fded63b
SuatAlpay <ssalpay96@gmail.com>
2022-02-10 14:50:18 -0500

    Added ConfigurationReader

--- src/test/java/googleTranslate/utilities/ConfigurationReader.java
+++ src/test/java/googleTranslate/utilities/ConfigurationReader.java
@@ -1,2 +1,29 @@
-package googleTranslate.utilities;public class ConfigurationReader {
+package googleTranslate.utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    private static Properties properties;
+
+    static {
+
+        try {
+            String path = "configuration.properties";
+            FileInputStream input = new FileInputStream(path);
+
+            properties = new Properties();
+            properties.load(input);
+
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static String getProperty(String keyName) {
+        return properties.getProperty(keyName);
+    }
 }
+
