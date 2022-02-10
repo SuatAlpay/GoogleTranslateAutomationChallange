a44f6b1de3715a6f1bb5c413ff870dfacf4b5c3d
SuatAlpay <ssalpay96@gmail.com>
2022-02-10 15:04:32 -0500

    Added PageFactory

--- src/test/java/googleTranslate/pages/GoogleTranslatePage.java
+++ src/test/java/googleTranslate/pages/GoogleTranslatePage.java
@@ -1,12 +1,13 @@
 package googleTranslate.pages;
 
 import googleTranslate.utilities.Driver;
+import org.openqa.selenium.devtools.v85.page.Page;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleTranslatePage {
 
     public GoogleTranslatePage() {
-
+        PageFactory.initElements(Driver.getDriver(),this);
     }
 }
