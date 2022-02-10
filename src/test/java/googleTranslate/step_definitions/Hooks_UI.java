e845a07a929150288bd35918e6372bb9bd05343a
SuatAlpay <ssalpay96@gmail.com>
2022-02-10 16:18:09 -0500

    Configured Hooks_UI

--- src/test/java/googleTranslate/step_definitions/Hooks_UI.java
+++ src/test/java/googleTranslate/step_definitions/Hooks_UI.java
@@ -1,7 +1,13 @@
 package googleTranslate.step_definitions;
 
+import googleTranslate.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
 
 public class Hooks_UI {
 
@@ -9,10 +15,21 @@ public class Hooks_UI {
     @Before
     public void setUpDriver(){
 
+        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        Driver.getDriver().manage().window().maximize();
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
 
+        if (scenario.isFailed()){
+
+            byte[] screenShot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenShot,"img/png", scenario.getName());
+        }
+
+
+        Driver.getDriver().close();
     }
 }
