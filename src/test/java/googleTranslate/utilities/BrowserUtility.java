1e9464aecc2a47a5f34aeaccb88664c958e4e744
SuatAlpay <ssalpay96@gmail.com>
2022-02-11 23:16:47 -0500

    Added waitFor & waitForClickable methods

--- src/test/java/googleTranslate/utilities/BrowserUtility.java
+++ src/test/java/googleTranslate/utilities/BrowserUtility.java
@@ -12,4 +12,20 @@ public class BrowserUtility {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(),timeToWait);
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
+
+    public static WebElement waitForClickable(WebElement element, int timeToWait){
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),timeToWait);
+
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void waitFor(int timeToWait){
+
+        try {
+            Thread.sleep(timeToWait * 1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 }
